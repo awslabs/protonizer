830856c3cabe72a6f7ce58464a44d49104525245
John Ritsema <john_ritsema@yahoo.com>
2023-03-24 18:13:11 -0400

    adds github actions ci workflow (#2)

---
+++ cmd/test/outputs.tf
@@ -0,0 +1,39 @@
+output "cluster_name" {
+	description = "this is cluster_name"
+  value = ""
+}
+
+output "cluster_arn" {
+	description = "this is cluster_arn"
+  value = ""
+}
+
+output "service_taskdef_execution_role" {
+	description = "this is service_taskdef_execution_role"
+  value = ""
+}
+
+output "vpc_id" {
+	description = "this is vpc_id"
+  value = ""
+}
+
+output "public_subnet_one_id" {
+	description = "this is public_subnet_one_id"
+  value = ""
+}
+
+output "public_subnet_two_id" {
+	description = "this is public_subnet_two_id"
+  value = ""
+}
+
+output "private_subnet_one_id" {
+	description = "this is private_subnet_one_id"
+  value = ""
+}
+
+output "private_subnet_two_id" {
+	description = "this is private_subnet_two_id"
+  value = ""
+}
